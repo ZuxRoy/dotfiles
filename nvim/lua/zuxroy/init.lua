6eadf618d91d6ccc0c36fe72ed598066486174d8
ZuxRoy <snehashis.roysr@gmail.com>
2024-08-03 19:29:12 +0530

    neovim cpp build trigger for cp

--- nvim/lua/zuxroy/init.lua
+++ nvim/lua/zuxroy/init.lua
@@ -14,3 +14,18 @@ vim.cmd('highlight Normal guibg=#000000')
 vim.cmd('highlight SignColumn guibg=#000000')
 vim.cmd('highlight LineNr term=bold ctermfg=DarkGrey ctermbg=NONE gui=NONE ')
 vim.cmd [[autocmd FileType * EmmetInstall]]
+
+-- cpp files competitive programming setup
+function CompileCppFile()
+  local filename = vim.api.nvim_buf_get_name(0)
+  local basename = vim.fn.fnamemodify(filename, ':t:r')
+  local command = 'g++ -o ' .. basename .. '.out ' .. filename
+  vim.cmd('! ' .. command)
+end
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'cpp',
+  callback = function()
+    vim.api.nvim_buf_set_keymap(0, 'n', '<F1>', ':lua CompileCppFile()<CR>', { noremap = true, silent = true })
+  end
+})
